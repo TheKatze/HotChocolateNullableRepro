61c4e37c61a45c6490abb71676d576a04d051dbb
Leo Katzengruber <leofel.katz@gmail.com>
2022-10-07 11:38:24 +0200

    more examples

--- Mutation.cs
+++ Mutation.cs
@@ -1,4 +1,6 @@
-﻿public class Mutation
+﻿using static Mutation;
+
+public class Mutation
 {
     public Task<Dictionary<string, int?>> Does_Work(Dictionary<string, int?> workItems)
     {
@@ -12,6 +14,23 @@
 
     public record WorkItem(string Id, string? Description);
     public Task<Dictionary<string, WorkItem?>> Does_Not_Work_Either(Dictionary<string, WorkItem?> workItems)
+    {
+        return Task.FromResult(workItems);
+    }
+
+    public Task<IEnumerable<KeyValuePair<string, WorkItem?>>> Another_Non_Working_Example(IEnumerable<KeyValuePair<string, WorkItem?>> workItems)
+    {
+        return Task.FromResult(workItems);
+    }
+
+    public record MyOwnKeyValuePair(string Key, WorkItem? Value);
+    public Task<IEnumerable<MyOwnKeyValuePair>> But_This_Workaround_Works(IEnumerable<MyOwnKeyValuePair> workItems)
+    {
+        return Task.FromResult(workItems);
+    }
+
+    public record GenericKVPair<TKey, TValue>(TKey Key, TValue Value);
+    public Task<IEnumerable<GenericKVPair<string, WorkItem?>>> With_Generics_It_Does_Not_Work(IEnumerable<GenericKVPair<string, WorkItem?>> workItems)
     {
         return Task.FromResult(workItems);
     }
